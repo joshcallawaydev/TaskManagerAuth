09079189a6257b9b541ac4a3a716d934f3d3b888
joshcallawaydev <joshrcal89@gmail.com>
2021-07-15 13:21:16 +0000

    Made changes to tasks visuals + added jquery.

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-
 import os
 from flask import (
     Flask, flash, render_template,
@@ -22,7 +21,7 @@ mongo = PyMongo(app)
 @app.route("/")
 @app.route("/get_tasks")
 def get_tasks():
-    tasks = mongo.db.tasks.find()
+    tasks = list(mongo.db.tasks.find())
     return render_template("tasks.html", tasks=tasks)
 
 
